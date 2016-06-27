cac1761afe6fff8195168c982573bd9f335024a4
Mhdl80 <Mahdi.shabanii80@gmail.com>
2016-06-27 19:28:11 +0430

    Update sticker2.lua

--- plugins/sticker2.lua
+++ plugins/sticker2.lua
@@ -9,7 +9,7 @@ local function tosticker(msg, success, result)
     redis:del("photo:sticker") 
   else 
     print('Error downloading: '..msg.id) 
-    send_large_msg(receiver, 'Failed, please try again!', ok_cb, false) 
+    send_large_msg(receiver, 'Failed,لطفا دوباره تلاش کنید!', ok_cb, false) 
   end 
 end 
 local function toimage(msg, success, result) 
@@ -23,7 +23,7 @@ local function toimage(msg, success, result)
     redis:del("sticker:photo") 
   else 
     print('Error downloading: '..msg.id) 
-    send_large_msg(receiver, 'Failed, please try again!', ok_cb, false) 
+    send_large_msg(receiver, 'Failed, لطفا دوباره تلاش کنید!', ok_cb, false) 
   end 
 end 
 
@@ -67,7 +67,7 @@ local function run(msg, matches)
 local url = 'http://latex.codecogs.com/png.latex?'..'\\dpi{'..size..'}%20\\huge%20\\'..font..'{{\\color{'..color..'}'..text..'}}' 
 local file = download_to_file(url,'file.webp') 
         send_document(msg.to.id , file, ok_cb, false) 
-        return "For Spherobot thanks to amirho3inf :)" 
+        return "For daredevilbot thanks :)" 
 end
 
     local receiver = get_receiver(msg) 
@@ -87,7 +87,7 @@ end
       elseif matches[1] == "photo" then 
        redis:get("sticker:photo") 
         load_document(msg.reply_id, toimage, msg) 
-     return 'For Spherobot thanks to amirho3inf :)' 
+     return 'For daredevilbot thanks :)' 
     end 
 end 
 
